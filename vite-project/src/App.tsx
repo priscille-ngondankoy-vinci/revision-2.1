a57fe7cb44d63ba720e4469dd40dc152f9a560f0
ngondankoy <priscille.ngondankoy@student.vinci.be>
2025-10-29 18:05:03 +0100

    rev ex2.2

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -13,25 +13,26 @@ const Header = (props: HeaderProps) => {
 };
 interface CinemaProps {
   name: string;
-  cinemaMovie1Title: string;
-  cinemaMovie1Director: string;
-  cinemaMovie2Title: string;
-  cinemaMovie2Director: string;
+  movies: Movie[];
+}
+interface Movie {
+  title: string;
+  director: string;
 }
 const Cinema = (props: CinemaProps) => {
   return (
     <div>
       <h2>{props.name}</h2>
       <ul>
-        <li>
-          <strong>{props.cinemaMovie1Title}</strong> - Réalisateur {" "}
-          {props.cinemaMovie1Director}
-        </li>
-        <li>
-          <strong>{props.cinemaMovie2Title}</strong> - Réalisateur {" "}
-          {props.cinemaMovie2Director}
-        </li>
+        {props.movies.map((movie, index) => (
+          <li key={index}>
+            <strong>{movie.title}</strong> - Réalisateur {" "}
+            {movie.director}
+          </li>
+        ))}
       </ul>
+        
+      
     </div>
   );
 }
@@ -40,38 +41,33 @@ const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
 
   const cinema1Name = "UGC DeBrouckère";
-  const cinema1Movie1Title = "Film 1 - DeBrouckère";
-  const cinema1Movie1Director = "Director A";
-  const cinema1Movie2Title = "Film 2 - DeBrouckère";
-  const cinema1Movie2Director = "Director B";
+
+  const movie1 = {
+    title: "HAIKYU-THE DUMPSTER BATTLE",
+    director: "Susumu Mitsunaka",
+  };
+  const movie2 = {
+    title: "GOODBYE JULIA ",
+    director: "Mohamed Kordofani",
+  };
 
   const cinema2Name = "UGC Toison d'Or";
-  const cinema2Movie1Title = "Film 1 - Toison d'Or";
-  const cinema2Movie1Director = "Director C";
-  const cinema2Movie2Title = "Film 2 - Toison d'Or";
-  const cinema2Movie2Director = "Director D";
+  const movie3 = {
+    title: "THE WATCHERS",
+    director: "Ishana Night Shyamalan",
+  };
+  const movie4 = {
+    title: "BAD BOYS: RIDE OR DIE",
+    director: "Adil El Arbi, Bilall Fallah",
+  };
 
   return (
     <div>
       <Header title={pageTitle} />
 
-      <div>
-        <Cinema name={cinema1Name}
-          cinemaMovie1Title={cinema1Movie1Title}
-          cinemaMovie1Director={cinema1Movie1Director}
-          cinemaMovie2Title={cinema1Movie2Title}
-          cinemaMovie2Director={cinema1Movie2Director}
-        />
-      </div>
+      <Cinema name={cinema1Name} movies={[movie1, movie2]} />
 
-      <div>
-        <Cinema name={cinema2Name}      
-          cinemaMovie1Title={cinema2Movie1Title}
-          cinemaMovie1Director={cinema2Movie1Director}
-          cinemaMovie2Title={cinema2Movie2Title}
-          cinemaMovie2Director={cinema2Movie2Director}
-        />
-      </div>
+      <Cinema name={cinema2Name} movies={[movie3, movie4]} />
     </div>
   );
 };
